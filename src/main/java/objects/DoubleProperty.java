267627d2fb0352b38c50afa11a004a5a59b55e47
samwalls <sam@s-w.io>
2017-11-25 11:11:59 +0000

    add basic primitive Property types

---
+++ src/main/java/objects/DoubleProperty.java
@@ -0,0 +1,19 @@
+package objects;
+
+public class DoubleProperty extends Property<Double> {
+
+    @Override
+    public long[] marshall(Double object) {
+        return new long[] { Double.doubleToLongBits(object) };
+    }
+
+    @Override
+    public Double unmarshall(long[] data) {
+        return Double.longBitsToDouble(data[0]);
+    }
+
+    @Override
+    public int size() {
+        return 1;
+    }
+}
---
+++ src/main/java/objects/IntProperty.java
@@ -0,0 +1,19 @@
+package objects;
+
+public class IntProperty extends Property<Integer> {
+
+    @Override
+    public long[] marshall(Integer object) {
+        return new long[] { object };
+    }
+
+    @Override
+    public Integer unmarshall(long[] data) {
+        return (int)data[0];
+    }
+
+    @Override
+    public int size() {
+        return 1;
+    }
+}
